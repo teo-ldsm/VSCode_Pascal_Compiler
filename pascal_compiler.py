fd6ac9db08636c81fd66cd5a4e9ad95b0f2e7f33
Teo <teolands31@gmail.com>
2023-10-21 15:23:14 +0200

    Initial commit

---
+++ pascal_compiler.py
@@ -0,0 +1,80 @@
+import os, sys, subprocess, re
+from pascal_error_displayer import *
+
+
+def check_error_message(message: str):
+    """Vérifie si le dernier message enregistré dans le fichier texte est identique à celui actuel.
+    Si c'est le cas, il arrête le programme et affiche le message d'erreur original"""
+
+    if not os.path.exists("./last_error.txt"):
+        save_error_message("")
+
+    with open("./last_error.txt", "r", encoding="UTF-8") as file:
+        if file.read() == message:
+            print("Vous avez compilé 2 fois le meme programme d'affilé. Affichage du message original\n\n"
+                  f"{message}")
+            exit(1)
+
+
+def save_error_message(message: str):
+    """Écrit dans un fichier le message d'erreur"""
+
+    with open("./last_error.txt", "w", encoding="UTF-8") as file:
+        file.write(message)
+
+
+args = sys.argv
+
+program_executable = f'{args[args.index("-p") + 1]}.exe'
+
+program_pascal = f'{args[args.index("-p") + 1]}.pas'
+
+file_path = args[args.index("-c") + 1]
+
+result = subprocess.check_output('tasklist', text=True)
+
+if file_path == ".":
+    print("Le fichier que vous essayez de compiler n'est pas enregistré\n"
+          "Faites Ctrl+S pour l'enregistrer puis réessayez")
+    exit(1)
+
+
+for line in result.splitlines():
+    if program_executable.lower() in line.lower():
+        parts = line.split()
+        process_name = parts[0]
+        process_pid = int(parts[1].strip())
+        print(f'Processus {process_name} (PID {process_pid}) est en cours d\'exécution. Fermeture en cours.')
+        subprocess.call(f"taskkill /F /PID {process_pid}")
+        
+
+process = subprocess.Popen(f"fpc \"{file_path}\\{program_pascal}\" -FE\"{file_path}\"",
+                           stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+stdout, stderr = process.communicate()
+
+if process.returncode == 0:
+    print("\n\n\n"
+          "Compilation réussie ! Appuyez sur Ctrl + Maj + T pour lancer le programme\n\n")
+    
+else:
+
+    print(f"\nSi ce message ne s'affiche pas correctement, relance la compilation 2 fois d'affilé pour "
+          f"afficher le message original\n\n")
+
+    print("La compilation a échoué. Erreurs de compilation :\n")
+
+    if f"Fatal: Cannot open file \"{program_pascal}\"" in stdout:
+        print("Free pascal n'as pas pu ouvrir votre fichier\n"
+              "Faites attention ! Le nom du fichier ne doit pas contenir d'espace ni de point virgule!\n")
+        exit(1)
+
+    check_error_message(stdout)
+    save_error_message(stdout)
+
+    error_lines = [i.removeprefix(program_pascal) for i in stdout.splitlines() if i.startswith(program_pascal) and not
+                   i.lower().endswith("errors compiling module, stopping")]
+
+    for error in error_lines:
+        print(Error(error))
+
+    exit(1)
---
+++ pascal_error_displayer.py
@@ -0,0 +1,99 @@
+class Error:
+
+    def __init__(self, error_text_brut: str):
+
+        self.error_text_brut = error_text_brut
+        self.program_name = ""
+        self.output_message = ""
+
+        self.error_location = error_text_brut[1:error_text_brut.index(")")].strip()
+
+        self.error_line = self.error_location[:self.error_location.index(",")]
+
+        self.error_type = error_text_brut[error_text_brut.index(")")+1:error_text_brut.index(":")].lower().strip()
+
+        self.error_text = error_text_brut[error_text_brut.index(":")+1:].lower().strip()
+
+    def set_program_name(self, program_name):
+        self.program_name = program_name
+
+    def error_analyser(self):
+        """Détermine quel est le type de l'erreur et décide de quelle fonction lancer"""
+
+        if (self.error_text.startswith("syntax error") and "expected" in self.error_text and
+                self.error_text.endswith("found")):
+
+            self.error_expected_found()
+
+        elif self.error_text.startswith("error: identifier not found"):
+            self.error_identifier_not_found()
+
+        else:
+            self.output_message += self.error_text_brut
+
+    def error_missing_semicolon(self):
+        self.output_message += (f"L\'erreur est a la ligne {self.error_line}. Tu as certainement oublié "
+                                f"de mettre un point virgule a la ligne juste avant.\n")
+
+    def error_semicolon_before_else(self):
+        self.output_message += ("Quand vous utilisez un if else, il n'y a pas de point virgule derrière le end "
+                                "qui précède le else\n"
+                                f"Enlevez le point virgule derrière le \"end\" juste avant la ligne {self.error_line}")
+
+    def error_identifier_not_found(self):
+        identifier = self.error_text[self.error_text.index("\""):self.error_text.rindex("\"")]
+
+        self.output_message += (f"Le mot clé {identifier} est inconnu. Si c'est une variable, vérifie qu'elle "
+                                f"est initialisée")
+
+    def error_missing_point_at_the_end(self):
+        self.output_message += "Il faut mettre un point derrière le tout dernier \"end\" de votre programme"
+
+    def error_illegal_expression(self):
+        pass
+
+    def error_expected_found(self):
+        expected = self.error_text[self.error_text.index(",")+3:self.error_text.index("expected")-2].strip().lower()
+        found = self.error_text[self.error_text.index("but")+5:self.error_text.index("found")-2].strip().lower()
+
+        self.output_message += f"Le programme attendait \"{expected}\" mais a trouvé \"{found}\" a la place.\n\n"
+
+        if expected == ".":
+            self.error_missing_point_at_the_end()
+
+        elif expected == ";":
+            if found == "else":
+                self.error_semicolon_before_else()
+
+            else:
+                self.error_missing_semicolon()
+
+        elif expected == ":":
+            if found == ")":
+                self.error_missing_parameter_type()
+
+            elif found == ";":
+                self.error_missing_var_or_function_type()
+
+    def error_missing_parameter_type(self):
+        self.output_message += ("Vous avez probablement oublié de définir le type d'un paramètre d'une fonction. \n"
+                                "Exemple: function NomFonction(Param1: Type; Param2: Type): TypeRenvoyéParLaFonction;")
+
+    def error_missing_var_or_function_type(self):
+        self.output_message += ("Vous avez probablement oublié de définir le type d'une variable ou d'une fonction. \n"
+                                "Exemle pour une variable: \n"
+                                "NomDeLaVariable: TypeDeLaVariable;\n\n"
+                                "Exemple pour une fonction:\n"
+                                "function NomFonction(Param1: Type; Param2: Type): TypeRenvoyéParLaFonction;")
+
+    def __repr__(self):
+
+        self.error_analyser()
+
+        return (f"Il y a une erreur a la ligne {self.error_location}\n"
+                f"{self.output_message}\n\n")
+
+
+if __name__ == '__main__':
+    erreur = Error("(273,9) Fatal: Syntax error, \";\" expected but \"identifier READLN\" found")
+    # erreur.error_analyser()
